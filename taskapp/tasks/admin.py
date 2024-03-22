100ca046f0af20a213dbd44687c144a19dfaaf89
Adrian De Guzman <adrian.deguzman@student.ateneo.edu>
2024-03-23 00:54:38 +0800

    Updated admin.py (simple)

--- taskapp/tasks/admin.py
+++ taskapp/tasks/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task, TaskGroup
+
+
+class TaskGroupAdmin(admin.ModelAdmin):
+    model = TaskGroup
+
+
+class TaskAdmin(admin.ModelAdmin):
+    model = Task
+
+
+admin.site.register(TaskGroup, TaskGroupAdmin)
+admin.site.register(Task, TaskAdmin)

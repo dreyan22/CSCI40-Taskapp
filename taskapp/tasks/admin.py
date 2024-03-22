71ddcfb869e5c68392fedf8019de88df261e6191
Adrian De Guzman <adrian.deguzman@student.ateneo.edu>
2024-03-23 02:53:31 +0800

    Updated admin.py

--- taskapp/tasks/admin.py
+++ taskapp/tasks/admin.py
@@ -3,12 +3,32 @@ from django.contrib import admin
 from .models import Task, TaskGroup
 
 
+class TaskInline(admin.TabularInline):
+    model = Task
+
+
 class TaskGroupAdmin(admin.ModelAdmin):
-    model = TaskGroup
+    inlines = [TaskInline,]
 
 
 class TaskAdmin(admin.ModelAdmin):
     model = Task
+
+    search_fields = ('name',)
+
+    list_display = ('name', 'due_date',)
+
+    list_filter = ('due_date', )
+
+    fieldsets = [
+        ('Details', {
+            
+            'fields': [
+                ('name', 'due_date'), 'taskgroup'
+            ]
+        }
+        ),
+    ]
 
 
 admin.site.register(TaskGroup, TaskGroupAdmin)

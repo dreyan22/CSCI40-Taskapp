bd538bd8024e7d56abad71f9b982601478fff7b3
Adrian De Guzman <adrian.deguzman@student.ateneo.edu>
2024-03-24 01:14:44 +0800

    Added password reset

--- taskapp/taskapp/settings.py
+++ taskapp/taskapp/settings.py
@@ -132,3 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/tasks/list'
 LOGOUT_REDIRECT_URL = '/tasks/list'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

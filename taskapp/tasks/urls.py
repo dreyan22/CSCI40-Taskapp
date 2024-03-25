d6b0c62acca54f1442b1ac0d6bd891260bcf9add
Adrian De Guzman <adrian.deguzman@student.ateneo.edu>
2024-03-25 12:51:59 +0800

    Updated views.py & urls.py  #Option1

--- taskapp/tasks/urls.py
+++ taskapp/tasks/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from .views import task_list, task_detail, index, TaskListView,TaskDetailView
 
 urlpatterns = [
-    # path('', task_list, name="task_list"),
+    # path('list', task_list, name="task_list"), # Option 1 for 5-Tasklist-Input
     # path('<int:pk>/detail', task_detail, name="task_detail"),
-    path('list', TaskListView.as_view(), name="task_list"),
-    path('<int:pk>/detail', TaskDetailView.as_view(), name="task_detail"),
-    path('', index, name='index')
+    path('', index, name='index'),
+    # path('list', TaskListView.as_view(), name="task_list"),
+    path('<int:pk>/detail', TaskDetailView.as_view(), name="task_detail")
 ]
 
 app_name = 'tasks'
--- taskapp/tasks/views.py
+++ taskapp/tasks/views.py
@@ -5,16 +5,27 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 
-from .models import Task
+from .models import Task, TaskGroup
 
 def index(request):
     return HttpResponse('Hello World! This came from the index view.')
 
+
+# Option 1 for 5-TaskList-Input
 def task_list(request):
     tasks = Task.objects.all()
     ctx = {
-        'tasks': tasks
+        'object_list': tasks,
+        'taskgroups': TaskGroup.objects.all()
     }
+    if request.method == 'POST':
+        task = Task()
+        task.name = request.POST.get('task_name')
+        task.due_date = request.POST.get('task_due')
+        task.taskgroup = TaskGroup.objects.get(pk=request.POST.get('taskgroup'))
+        task.save()
+
+
     return render(request, "task_list.html", ctx)
 
 def task_detail(request, pk):
